5df4b9cf605242c9122b30ef4db911dfe33632c1
gospau <57227396+gospau@users.noreply.github.com>
2020-12-10 22:51:47 -0600

    4th project, creating a form to pull in file and distrubuting what is needed of.

---
+++ Pau_Go_PA4/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Pau_Go_PA4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(786, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(126, 26);
+            this.openToolStripMenuItem.Text = "open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.LawnGreen;
+            this.richTextBox1.Location = new System.Drawing.Point(404, 48);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(359, 390);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(67, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "For Caramel Candy";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(67, 133);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(283, 33);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Fruity candy with sugar % greater than 0.5";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(67, 183);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(215, 28);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Hard candy\'s average suger %";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(67, 237);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(303, 27);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Maximum win % of crisped rice wafer candies";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(67, 285);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(173, 26);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Highest Win/Price Ratio";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOrange;
+            this.ClientSize = new System.Drawing.Size(786, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Pau_Go_PA4/Form1.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Pau_Go_PA4
+{
+    public partial class Form1 : Form
+    {
+        private string filepath;
+        public Form1()
+        {
+            InitializeComponent();
+            filepath = "";
+        }
+
+        private void OpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = openFileDialog1.ShowDialog();
+
+            if (dr == DialogResult.OK)
+            {
+                filepath = openFileDialog1.FileName;
+            }
+            else
+            {
+                filepath = "";
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (filepath == "")
+            {
+                MessageBox.Show("Select a file first");
+                return;
+            }
+
+            int countCaramel = 0;
+
+            using (var sr = new StreamReader(filepath))
+            {
+                while (sr.EndOfStream != true)
+                {
+                    string line = sr.ReadLine();
+
+                    if (line.StartsWith("competitorname"))
+                    {
+                        continue;
+                    }
+
+                    string[] data = line.Split(',');
+
+                    string caramel;
+                    try
+                    {
+                        caramel = data[3];
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Please select the correct file");
+                        return;
+                    }
+
+
+                    if (caramel == "1")
+                    {
+                        countCaramel++;
+                    }
+                }
+            }
+
+            richTextBox1.Text = String.Format("Counted {0} candies with caramel\n\n", countCaramel);
+
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (filepath == "")
+            {
+                MessageBox.Show("Select a file first");
+                return;
+            }
+
+            List<string> fruityList = new List<string>();
+            var sugarper = new List<double>();
+
+            using (var sr = new StreamReader(filepath))
+            {
+                while (sr.EndOfStream != true)
+                {
+                    string line = sr.ReadLine();
+
+                    if (line.StartsWith("competitorname"))
+                    {
+                        continue;
+                    }
+
+                    string[] data = line.Split(',');
+
+                    string name = data[0];
+
+                    string fruity;
+                    try
+                    {
+                        fruity = data[2];
+                    }
+                    catch
+                    {
+                        MessageBox.Show("That is the wrong file");
+                        return;
+                    }
+
+                    double sugar = double.Parse(data[10]);
+                    if (fruity == "1" && sugar > .5)
+                    {
+                        fruityList.Add(name);
+                        sugarper.Add(sugar);
+                    }
+                }
+            }
+
+
+            richTextBox1.AppendText(String.Format("Fruity sweet candies:\n"));
+
+            for (int i = 0; i < fruityList.Count; i++)
+            {
+
+                richTextBox1.AppendText(String.Format("{0} - {1:F}\n", fruityList[i], sugarper[i]));
+
+            }
+            richTextBox1.AppendText(String.Format("\n"));
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if (filepath == "")
+            {
+                MessageBox.Show("Select a file first");
+                return;
+            }
+
+            int counthard = 0;
+            var sugarpert = new List<double>();
+
+            using (var sr = new StreamReader(filepath))
+            {
+                while (sr.EndOfStream != true)
+                {
+                    string line = sr.ReadLine();
+
+                    if (line.StartsWith("competitorname"))
+                    {
+                        continue;
+                    }
+
+                    string[] data = line.Split(',');
+
+                    string hard;
+                    try
+                    {
+                        hard = data[7];
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Wrong File!");
+                        return;
+                    }
+
+                    double hardp = double.Parse(data[10]);
+
+                    if (hard == "1")
+                    {
+                        counthard++;
+                        sugarpert.Add(hardp);
+                    }
+                }
+            }
+
+            double total = sugarpert.Sum() / counthard;
+
+            richTextBox1.AppendText(String.Format("Average sugar % in hard candies: {0:F}\n\n", total));
+
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            if (filepath == "")
+            {
+                MessageBox.Show("Select a file first");
+                return;
+            }
+
+            double maxwin = double.MinValue;
+            string maxwinN = "";
+
+            using (var sr = new StreamReader(filepath))
+            {
+                while (sr.EndOfStream != true)
+                {
+                    string line = sr.ReadLine();
+
+                    if (line.StartsWith("competitorname"))
+                    {
+                        continue;
+                    }
+
+                    string[] data = line.Split(',');
+
+
+                    string name = data[0];
+
+                    string crw;
+                    try
+                    {
+                        crw = data[6];
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Wrong File!");
+                        return;
+                    }
+
+                    double win = double.Parse(data[12]);
+
+                    if (crw == "1" && maxwin < win)
+                    {
+                        maxwin = win;
+                        maxwinN = name;
+                    }
+
+
+
+                }
+            }
+            richTextBox1.AppendText(String.Format("{0} is the wafer with highest win percentae of {1:F}\n\n", maxwinN, maxwin));
+
+
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            if (filepath == "")
+            {
+                MessageBox.Show("Select a file first");
+                return;
+            }
+
+            double maxratio = 0;
+            string maxname = "";
+
+            using (var sr = new StreamReader(filepath))
+            {
+                while (sr.EndOfStream != true)
+                {
+                    string line = sr.ReadLine();
+
+                    if (line.StartsWith("competitorname"))
+                    {
+                        continue;
+                    }
+
+                    string[] data = line.Split(',');
+
+
+                    string name = data[0];
+
+                    double priceper;
+                    try
+                    {
+                        priceper = double.Parse(data[11]);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("WRONG File!");
+                        return;
+                    }
+
+                    double winper = double.Parse(data[12]);
+
+                    double total = winper / priceper;
+                    if (total > maxratio)
+                    {
+                        maxratio = total;
+                        maxname = name;
+
+                    }
+
+
+                }
+
+                richTextBox1.AppendText(String.Format("Candy with hightest Win/Price ratio:\n"));
+                richTextBox1.AppendText(String.Format("> {0:F}, {1}\n\n", maxratio, maxname));
+            }
+        }
+    }
+}
